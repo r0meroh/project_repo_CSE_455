d9415847ef95257bfec79614e5f4267e419028e3
hector27 <hector27c7@gmail.com>
2020-09-08 13:00:36 -0700

    added Navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 const Navbar1 = () => {
   return (
     <>
       <Navbar bg='dark' variant='dark'>
         <Navbar.Brand href='#home'>Detectives</Navbar.Brand>
         <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
-          <Nav.Link href='#features'>Features</Nav.Link>
-          <Nav.Link href='#pricing'>Pricing</Nav.Link>
+          <Nav.Link as={NavLink} to='/' exact>
+            {' '}
+            Home{' '}
+          </Nav.Link>
+          <Nav.Link as={NavLink} to='/graph' exact>
+            graph
+          </Nav.Link>
         </Nav>
       </Navbar>
     </>
