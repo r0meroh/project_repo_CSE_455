2c8a9d428850d49acd19be9c9aed4b29920ae92d
Alfredo <45499181+Alfredo9712@users.noreply.github.com>
2020-09-08 12:40:10 -0700

    Navbar addition

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 const Navbar1 = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand href='#home'>Detectives</Navbar.Brand>
+        <Nav className='mr-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#features'>Features</Nav.Link>
+          <Nav.Link href='#pricing'>Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+    </>
+  );
 };
 
 export default Navbar1;
