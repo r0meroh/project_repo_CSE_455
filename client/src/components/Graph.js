6c2994d19307fc09152bd5daf375dcb072d45c77
Tj <Tj.Sandhu17@hotmail.com>
2020-09-09 14:45:05 -0700

    Testing out Line Graphs

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import { Paper } from "@material-ui/core";
+import {
+  ArgumentAxis,
+  ValueAxis,
+  Chart,
+  LineSeries,
+} from "@devexpress/dx-react-chart-material-ui";
 const Graph = () => {
+  const data = [
+    { argument: 1, value: 100 },
+    { argument: 2, value: 90 },
+    { argument: 3, value: 30 },
+  ];
+
   return (
-    <div>
-      <h1>Graph page</h1>
-    </div>
+    <Paper>
+      <Chart data={data}>
+        <ArgumentAxis />
+        <ValueAxis />
+
+        <LineSeries valueField="value" argumentField="argument" />
+      </Chart>
+    </Paper>
   );
 };
 
