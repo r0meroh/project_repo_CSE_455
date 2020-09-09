e5a3330459bde8268134954cdee9ed874284c58e
Alfredo <45499181+Alfredo9712@users.noreply.github.com>
2020-09-09 14:52:37 -0700

    More chart testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import "./App.css";
-import Navbar1 from "./components/Navbar";
-import Landing from "./components/Landing";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Graph from "./components/Graph";
-import About_Us from "./components/About_Us";
-import "@devexpress/dx-react-chart-bootstrap4/dist/dx-react-chart-bootstrap4.css";
+import './App.css';
+import Navbar1 from './components/Navbar';
+import Landing from './components/Landing';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Graph from './components/Graph';
+import About_Us from './components/About_Us';
+//import "@devexpress/dx-react-chart-bootstrap4/dist/dx-react-chart-bootstrap4.css";
 
 function App() {
   return (
@@ -15,13 +15,13 @@ function App() {
       <div>
         <Navbar1 />
         <Switch>
-          <Route path="/graph">
+          <Route path='/graph'>
             <Graph />
           </Route>
-          <Route path="/about_us">
+          <Route path='/about_us'>
             <About_Us />
           </Route>
-          <Route path="/">
+          <Route path='/'>
             <Landing />
           </Route>
         </Switch>
