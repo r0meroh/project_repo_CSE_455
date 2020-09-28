1e0e478cebfaeb4a49886e7e00a163182ba51661
Tj <Tj.Sandhu17@hotmail.com>
2020-09-27 17:45:59 -0700

    New Landing Page V1

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,51 +1,40 @@
 import React, { useState, Fragment } from "react";
-import { Carousel } from "react-bootstrap";
-import { Container } from "react-bootstrap";
+import {
+  Carousel,
+  Container,
+  Jumbotron,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
 const Landing = () => {
   return (
-    <div className="">
-      <Carousel className="carousel" style={{ position: "absolute", top: 0 }}>
-        <Carousel.Item>
-          <Fragment>
-            <img
-              className="d-inline-block w-100 min-vh-100"
-              src="https://cdn.pixabay.com/photo/2020/03/16/16/29/virus-4937553_960_720.jpg"
-              alt="First slide"
-            />
-          </Fragment>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-auto"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
+    <Container fluid>
+      <Row className="Jumbotron">
+        <Col>
+          <Jumbotron>
+            <h1>Hello, world!</h1>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
             </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+        </Col>
+      </Row>
+      <Row className="BoxText">
+        <Col>
+          <h1>TEXT CONTENT</h1>
+        </Col>
+      </Row>
+      <Row className="Footer">
+        <Col>
+          <h1>Footer</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
