e8735591c9c339e969ea3b47b660df10133fa7dd
hector27 <hector27c7@gmail.com>
2020-09-30 13:34:22 -0700

    About Us Updated

--- client/src/components/About_Us.js
+++ client/src/components/About_Us.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import './About_Us.css';
 
 const About_Us = () => {
+  const data = [
+    {
+      name: 'About Us',
+      description: 'Coded',
+      photo:
+        'https://www.statnews.com/wp-content/uploads/2020/02/Coronavirus-CDC-768x432.jpg',
+    },
+  ];
+
+  // put image into a canvas then set parameters
+  // or should we have preset
   return (
-    <div>
-      <h1> About us page! </h1>
-    </div>
+    <>
+      <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+      </Jumbotron>
+
+      <div className='card'>
+        {data.map((profile) => (
+          <div>
+            <img src={profile.photo} width='300' height='180'></img>
+            <p>{profile.description}</p>
+            <h1>{profile.name}</h1>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
