12a9b1be36b21f3252dcac1c314af60e0f3aec51
hector27 <hector27c7@gmail.com>
2020-10-06 10:13:30 -0700

    About Us Update 3

--- client/src/components/About_Us.js
+++ client/src/components/About_Us.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
-
 import './About_Us.css';
 
 const About_Us = () => {
@@ -29,41 +28,40 @@ const About_Us = () => {
     {
       name: 'Hector Carbajal',
       description: 'description ',
-      photo:
-        'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/105591019_3076225415787781_2688025347582234513_n.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=LT9sQW2-c_MAX-HCQbI&_nc_ht=scontent-lax3-1.xx&oh=6cabeccf9147d66ba819b1afe1b8105a&oe=5F9B0866',
+      photo: 'https://i1.sndcdn.com/avatars-000605914911-n6offh-t500x500.jpg',
     },
   ];
 
   return (
     <>
       <div className='about_us_header'>
-        <Jumbotron>
-          <h1>Hello, world!</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
-        </Jumbotron>
+        <h1 className='header_about_us'>About Us</h1>
+        <h4>
+          Lorem ipsum dolor sit amet elit scelerisque.Commodo sed egestas
+          egestas fringilla. Vulputate mi sit amet mauris commodo quis imperdiet
+          massa. Euismod nisi porta lorem mollis.
+        </h4>
       </div>
 
       <Container className='team'>
+        <div className='meet_the_team'>
+          <h1> Meet the Team</h1>
+        </div>
         {data.map((profile) => (
-          <div className='single_card'>
+          <div className='cards'>
             <Row>
-              <Col>
-                <img src={profile.photo} width='250' height='250'></img>
+              <Col xs={6} md={4} className='photoname'>
+                <img src={profile.photo} width='300' height='300'></img>
                 <h2>{profile.name}</h2>
               </Col>
-              <Col className='descript'>
-                <h4>{profile.description}</h4>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <h2></h2>
-              </Col>
-              <Col>
-                <h2>Social Medias to be Provided</h2>
+
+              <Col xs={12} md={8}>
+                <Row>
+                  <h2>{profile.description}</h2>
+                </Row>
+                <Row>
+                  <h2>Add Socials/Contact information</h2>
+                </Row>
               </Col>
             </Row>
           </div>
