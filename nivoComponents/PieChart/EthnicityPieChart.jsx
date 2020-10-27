a6e0a884c628b5f1b6644b04677453d1e724d4ad
cocopuffcodes <69946336+cocopuffcodes@users.noreply.github.com>
2020-10-27 04:17:45 -0700

    Ethnicity Pie Chart
    Displays the extent of impact from COVID-19 experienced from specific members of ethnic groups through percentages visualized in pie chart format.

---
+++ nivoComponents/PieChart/EthnicityPieChart.jsx
@@ -0,0 +1,66 @@
+import React, {useEffect} from 'react'
+import { ResponsivePie } from '@nivo/pie'
+import {renderPieChart} from '../../redux/actions/pieActions'
+import { useSelector, useDispatch } from 'react-redux';
+
+const PieChart = () => {
+
+    const ethnicityPie = useSelector((state) => state.ethnicityPie);
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(renderPieChart())
+    }, [dispatch] );
+    return (
+        <>
+        <ResponsivePie
+        data={ethnicityPie.data}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        colors={{ scheme: 'nivo' }}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={6}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={16}
+        radialLabelsLinkHorizontalLength={24}
+        radialLabelsLinkStrokeWidth={1}
+        radialLabelsLinkColor={{ from: 'color' }}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#333333"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        defs={[
+            {
+                id: 'dots',
+                type: 'patternDots',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                size: 4,
+                padding: 1,
+                stagger: true
+            },
+            {
+                id: 'lines',
+                type: 'patternLines',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10
+            }
+        ]}
+  
+    />
+        </>
+    )
+}
+
+export default PieChart
+
+
